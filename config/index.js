7c259d5669a8a1b0973755a3880548ba04b4f4e3
郭涛 <35476411+Taogreat@users.noreply.github.com>
2022-09-21 17:24:24 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,9 +62,40 @@ export const config = {
       // 想要发送的人的名字
       name: "粑粑欢",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "osibX5q3sg3bIl9e9WOEsclFHBJ0",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "lLj99ZS0HHvnF-QUOhxH9wCauSGlV0ql50E0CHTPCcE",
+      // 所在省份
+      province: "四川",
+      // 所在城市
+      city: "成都",
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '12-08',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "/",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+    
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {"type": "生日", "name": "粑粑欢", "year": "1997", "date": "12-08"},
+       // {"type": "节日", "name": "在一起的日子", "year": "2020", "date": "10-01"},
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        {"keyword": "love_day", date: "2020-10-01"},
+      ]
+    },
+    {
+      // 想要发送的人的名字
+      name: "粑粑欢",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "osibX5kAqJkqFWx6id4wbQ6nfiFM",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "35dbqCYxkBLiAJsbiI1ewG5mluu6xmDUNbDkV6JbNTc",
+      useTemplateId: "lLj99ZS0HHvnF-QUOhxH9wCauSGlV0ql50E0CHTPCcE",
       // 所在省份
       province: "四川",
       // 所在城市
